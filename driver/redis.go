9eb73373c3624f81a31e5ee90d3c4ce5fae6f049
Harshit <raj.harshitsinha08@gmail.com>
2025-05-14 23:56:44 +0530

    new changes for render config related to deployment

--- driver/redis.go
+++ driver/redis.go
@@ -2,14 +2,28 @@ package driver
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedis(hostname string) (*redis.Client, error) {
+
+	_ = godotenv.Load()
+
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisHost == "" {
+		redisHost = "redis" // fallback for local development
+	}
+	if redisPort == "" {
+		redisPort = "6379" // fallback for local development
+	}
 	var rdb = redis.NewClient(&redis.Options{
-		Addr:     hostname,
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
 		Password: "", // no password by default
 		DB:       0,  // use default DB
 	})
